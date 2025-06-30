b7776230a0c20df7d0aa1ee756cdce7b14cda0df
TorresAWS <d.torres.rangel@gmail.com>
2025-06-30 15:41:28 -0400

    Delete services/emailforwarder/sns-emailforwarder/backend-exported-from-global-roles-lambda-sqs.tf
    rm export from sqs role in services emailforwarder sns

--- services/emailforwarder/sns-emailforwarder/backend-exported-from-global-roles-lambda-sqs.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "general-roles-lambda-sqs" {
-   backend = "s3"
-   config = {
-        key = "global/roles/lambda-sqs/lambda-sqs.tfstate"
-        region = "us-east-1"
-        profile  = "Infrastructure"
-        bucket  = "tfstate05122025" 
-   }
-}
