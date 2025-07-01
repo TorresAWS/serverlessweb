b06f94049a9eb733948c980516b2bdc1195d05e6
TorresAWS <d.torres.rangel@gmail.com>
2025-07-01 10:20:33 -0400

    Update output.tf

--- global/variables/output.tf
+++ global/variables/output.tf
@@ -6,7 +6,7 @@ output "account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 output "region" {
-  value = data.aws_region.current.name
+  value = data.aws_region.current.region
 }
 output "backendname" {
   value = var.backendname
